bf8e199910ad1b8dd9c1473e36da950a4814b9ab
Zolianul <vladcroicu@yahoo.com>
2022-05-25 16:19:41 +0300

    Ummmm UI

--- src/main/java/controllers/StageController.java
+++ src/main/java/controllers/StageController.java
@@ -90,11 +90,39 @@ public class StageController {
         stage.show();
     }
     public void switchToScene9(ActionEvent event) throws IOException {
-        root = FXMLLoader.load(getClass().getResource("laststage.fxml"));
+        root = FXMLLoader.load(getClass().getResource("endslide01.fxml"));
+        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+    public void switchToScene10(ActionEvent event) throws IOException {
+        root = FXMLLoader.load(getClass().getResource("endslide02.fxml"));
         stage = (Stage)((Node)event.getSource()).getScene().getWindow();
         scene = new Scene(root);
         stage.setScene(scene);
         stage.show();
     }
 
+    public void switchToScene11(ActionEvent event) throws IOException {
+        root = FXMLLoader.load(getClass().getResource("endslide03.fxml"));
+        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+    public void switchToScene12(ActionEvent event) throws IOException {
+        root = FXMLLoader.load(getClass().getResource("endslide04.fxml"));
+        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+    public void switchToScene13(ActionEvent event) throws IOException {
+        root = FXMLLoader.load(getClass().getResource("laststage.fxml"));
+        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
 }
